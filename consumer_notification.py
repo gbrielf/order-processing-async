5e1a92d7721f93b3743b7bf60ab227b958f7c709
Gabriel Henrique F. Barbosa <56904537+gbrielf@users.noreply.github.com>
2025-12-11 13:24:54 +0000

    feat: consumer_notification finalizado

---
+++ consumer_notification.py
@@ -0,0 +1,50 @@
+import pika
+import json
+import time
+
+RABBITMQ_HOST = 'localhost'
+QUEUE_NAME = 'pedidos_pendentes'
+
+def callback_notification(ch, method, properties, body):
+    # Callback chamado quando uma mensagem é recebida na fila
+
+    order = json.loads(body)
+    order_id = order.get('order_id', 'N/A')
+
+    print(f"[Notificação] Recebida pelo ID: {order_id} - Preparando e-mail...")
+
+    #simulação de Processamento Rápido
+    time.sleep(1)
+
+    # Lógica de negócio (envio de e-mail)
+    print(f"[Notificação] >>> E-mail de confirmação enviado para o Pedido ID {order_id}.")
+
+    # ACK (Acknowledgment) - Confirmação ao MOM
+    ch.basick_ack(delivery_tag = method.delivery_tag)
+    print(f"[Notificação] ACK enviado para Pedido ID: {order_id}")
+
+def start_consumer_notification():
+    print('[Notificação] Tentando conectar ao RabbitMQ...')
+    
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=RABBITMQ_HOST)
+    )
+    channel = connection.channel()
+
+    # Garantir que a fila exista
+    channel.queue_declare(queue=QUEUE_NAME, durable=True)
+
+    # Configurar para receber apenas 1 mensagem por vez (QoS)
+    channel.basic_qos(prefetch_count=1)
+
+    # Assinar a fila
+    channel.basic_consume(
+        queue=QUEUE_NAME,
+        on_message_callback=callback_notification
+    )
+
+    print('[Notificação] [*] Aguardando pedidos. Pressione CNTRL+C para sair.')
+    channel.start_consuming()
+
+if __name__ == '__main__'
+    start_consumer_notification()
